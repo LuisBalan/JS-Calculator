d00a0c7754898d7828a20c6ad5f4a5066690940d
LuisBalan <jlbalcz@gmail.com>
2022-04-03 17:06:44 -0500

    set selectors

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const digit = document.querySelectorAll('number-key');
+const operator = document.querySelector('operator-key');
+const equals = document.querySelector('equals-key');
+const clear = document.getElementById('clear-key')
+
