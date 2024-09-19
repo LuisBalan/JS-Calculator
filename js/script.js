af792584c4d27c48dbb3e29dfb54a45537bd3633
LuisBalan <jlbalcz@gmail.com>
2024-09-19 13:48:05 -0600

    add test comment

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ const digit = document.querySelectorAll('number-key');
 const operator = document.querySelector('operator-key');
 const equals = document.querySelector('equals-key');
 const clear = document.getElementById('clear-key')
-
+// this is a comment
